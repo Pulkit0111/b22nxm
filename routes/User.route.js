a4efa704f2bcf7fe48bb2a1226bab72ec09d9481
Pulkit0111 <ptyagi1551@gmail.com>
2023-01-13 21:46:33 +0530

    deploy

---
+++ routes/User.route.js
@@ -0,0 +1,54 @@
+const express=require("express")
+const {UserModel}=require("../models/User.model")
+const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
+require("dotenv").config()
+
+const userRouter=express.Router()
+
+userRouter.post("/register",async (req,res)=>{
+    const {email,pass,name,age}=req.body
+    try{
+        bcrypt.hash(pass, 5, async (err, secure_password) => {
+            if(err){
+                console.log(err)
+            } else{
+                const user=new UserModel({email,pass:secure_password,name,age})
+            await user.save()
+            res.send("Registered")
+            }
+        });
+    }catch(err){
+        res.send("Error in registering the user")
+        console.log(err)
+    }
+})
+
+userRouter.post("/login",async (req,res)=>{
+    const {email,pass}=req.body
+    console.log(email,pass)
+    try{
+        const user=await UserModel.find({email})
+        const hashed_pass=user[0].pass
+        if(user.length>0){
+            bcrypt.compare(pass, hashed_pass, (err, result) => {
+                if(result){
+                    const token = jwt.sign({userID:user[0]._id}, process.env.key);
+                    res.send({"msg":"Login Successfull","token":token})
+                } else {
+                    res.send("Wrong Credntials")
+                }
+            });
+        } else {
+            res.send("Wrong Credntials")
+        }
+    } catch(err){
+        res.send("Something went wrong")
+        console.log(err)
+    }
+    
+})
+
+module.exports={
+    userRouter
+}
